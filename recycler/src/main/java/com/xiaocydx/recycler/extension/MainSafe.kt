93283cf29336f000791549e90476942f7b4c9731
xcc <xiaocydx@163.com>
2022-03-19 22:07:24 +0800

    feat(Recycler): 添加等待下一帧绘制完成的函数

--- recycler/src/main/java/com/xiaocydx/recycler/extension/MainSafe.kt
+++ recycler/src/main/java/com/xiaocydx/recycler/extension/MainSafe.kt
@@ -1,21 +1,21 @@
 package com.xiaocydx.recycler.extension
 
+import android.os.Handler
 import android.os.Looper
+import android.view.Choreographer
 import android.view.View
 import androidx.core.os.HandlerCompat
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import java.lang.Runnable
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
 
 /**
  * 主线程异步消息Handler
  */
-private val mainHandler = HandlerCompat.createAsync(Looper.getMainLooper())
+private val asyncHandler = HandlerCompat.createAsync(Looper.getMainLooper())
 
 /**
  * 当前是否为主线程
@@ -28,7 +28,7 @@ internal val isMainThread: Boolean
  */
 internal inline fun runOnMainThread(crossinline action: () -> Unit) {
     if (!isMainThread) {
-        mainHandler.post { action() }
+        asyncHandler.post { action() }
     } else {
         action()
     }
@@ -42,19 +42,40 @@ internal fun assertMainThread() {
 }
 
 /**
- * 发送同步消息，并等待被执行
+ * 在下一帧绘制完成后执行[action]
  */
-internal suspend fun View.awaitPost() {
-    suspendCancellableCoroutine<Unit> suspend@{
-        val action = Runnable { it.resume(Unit) }
-        if (!post(action)) {
-            it.resumeWithException(CancellationException("发送消息失败。"))
-            return@suspend
-        }
-        it.invokeOnCancellation { removeCallbacks(action) }
+internal fun View.doOnFrameComplete(action: () -> Unit): Disposable {
+    if (!isAttachedToWindow) {
+        return emptyDisposable()
+    }
+    return if (isLayoutRequested) {
+        // 已经申请重绘，发送同步消息，等待下一帧同步屏障解除后被执行
+        FrameCompleteObserver(postFrame = false, handler, action)
+    } else {
+        // 下一帧doFrame()的执行过程可能会申请重绘，添加同步屏障，
+        // 因此发送异步消息，避免被同步屏障影响，无法在当前帧被执行。
+        FrameCompleteObserver(postFrame = true, asyncHandler, action)
     }
 }
 
+/**
+ * 等待下一帧绘制完成
+ */
+internal suspend fun View.awaitFrameComplete() {
+    suspendCancellableCoroutine<Unit> { continuation ->
+        val disposable = doOnFrameComplete {
+            continuation.resume(Unit)
+        }
+        continuation.invokeOnCancellation {
+            disposable.dispose()
+        }
+    }
+}
+
+/**
+ * 将Flow的执行上下文的调度器更改为主线程调度器，
+ * 若执行上下文已经包含主线程调度器，则不调用[flowOn]。
+ */
 internal fun <T> Flow<T>.flowOnMain(
     mainDispatcher: MainCoroutineDispatcher = Dispatchers.Main.immediate
 ): Flow<T> = flow {
@@ -63,4 +84,36 @@ internal fun <T> Flow<T>.flowOnMain(
         else -> this@flowOnMain.flowOn(mainDispatcher)
     }
     flow.collect { emit(it) }
+}
+
+private class FrameCompleteObserver(
+    postFrame: Boolean,
+    handler: Handler,
+    action: () -> Unit
+) : Disposable, Runnable {
+    private var handler: Handler? = handler
+    private var action: (() -> Unit)? = action
+    override val isDisposed: Boolean
+        get() = handler == null && action == null
+
+    init {
+        if (postFrame) {
+            Choreographer.getInstance().postFrameCallback {
+                handler.post(this)
+            }
+        } else {
+            handler.post(this)
+        }
+    }
+
+    override fun run() {
+        action?.invoke()
+        dispose()
+    }
+
+    override fun dispose() = runOnMainThread {
+        handler?.removeCallbacks(this)
+        handler = null
+        action = null
+    }
 }
