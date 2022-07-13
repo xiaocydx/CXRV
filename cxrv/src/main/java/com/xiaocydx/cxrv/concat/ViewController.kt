aecd2856d49282661ce3e3ea170a39e8cadaaa2c
xcc <xiaocydx@163.com>
2022-07-13 18:26:05 +0800

    fix: 修复ViewPager2场景下出现的内存泄漏问题

--- cxrv/src/main/java/com/xiaocydx/cxrv/concat/ViewController.kt
+++ cxrv/src/main/java/com/xiaocydx/cxrv/concat/ViewController.kt
@@ -7,116 +7,69 @@ import androidx.annotation.VisibleForTesting
 import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView.*
 import androidx.recyclerview.widget.RecyclerView.RecycledViewPool.ScrapData
+import androidx.recyclerview.widget.ViewController.Tracker
 
 /**
  * 从[Recycler]中清除ViewHolder的控制器
  *
- * ### [onDetachedFromRecyclerView]
- * 当Adapter从RecyclerView上分离时，清除已分离的[viewHolder]，
- * 若Adapter是被[ConcatAdapter]移除，则需要拦截要被回收的[viewHolder]，
+ * ### [Adapter.onDetachedFromRecyclerView]
+ * 当Adapter从RecyclerView上分离时，清除已分离的[Tracker.viewHolder]，
+ * 若Adapter是被[ConcatAdapter]移除，则需要拦截要被回收的[Tracker.viewHolder]，
  * 拦截流程为[makeViewHolderRecycleFailed]和[onFailedToRecycleView]。
  *
- * ### [onViewDetachedFromWindow]
- * 当RecyclerView从Window上分离时，清除已分离的[viewHolder]，
- * 避免共享[RecycledViewPool]的场景回收无用的[viewHolder]。
+ * ### [View.OnAttachStateChangeListener.onViewDetachedFromWindow]
+ * 当RecyclerView从Window上分离时，清除已分离的[Tracker.viewHolder]，
+ * 避免共享[RecycledViewPool]的场景回收无用的[Tracker.viewHolder]。
  *
  * @author xcc
  * @date 2021/10/15
  */
-internal class ViewController : View.OnAttachStateChangeListener {
-    private var viewType = INVALID_TYPE
+internal class ViewController {
+    private val tracker = Tracker()
     private var View.hasTransientState: Boolean
         get() = ViewCompat.hasTransientState(this)
         set(value) = ViewCompat.setHasTransientState(this, value)
-    private val isAdapterAttached: Boolean
-        get() = recyclerView != null
-    var viewHolder: ViewHolder? = null
-        private set
-    var recyclerView: RecyclerView? = null
-        private set
+    val viewHolder: ViewHolder?
+        get() = tracker.viewHolder
+    val recyclerView: RecyclerView?
+        get() = tracker.recyclerView
 
     fun onBindViewHolder(holder: ViewHolder) {
-        viewHolder = holder
-        saveItemViewType(holder)
+        tracker.track(holder)
     }
 
     fun onViewRecycled(holder: ViewHolder) {
-        viewHolder = null
-        saveItemViewType(holder)
-        recyclerView?.setMaxRecycledViews()
+        tracker.untrack(holder)
+        recyclerView?.let(tracker::limitMaxScrap)
     }
 
     fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
-        this.recyclerView = recyclerView
-        recyclerView.addOnAttachStateChangeListener(this)
+        tracker.track(recyclerView)
     }
 
     fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
-        this.recyclerView = null
-        recyclerView.removeOnAttachStateChangeListener(this)
+        tracker.untrack(recyclerView)
         recyclerView.clearDetachedViewHolder()
-        makeViewHolderRecycleFailed()
-    }
-
-    override fun onViewAttachedToWindow(view: View) {
-    }
-
-    override fun onViewDetachedFromWindow(view: View) {
-        (view as? RecyclerView)?.clearDetachedViewHolder()
-    }
-
-    /**
-     * 当[ConcatAdapter]使用隔离ViewType配置时，[Adapter.getItemViewType]获取到的是本地ViewType，
-     * 并不是[RecycledViewPool]中的ViewType，因此通过[SimpleViewHolder.getItemViewType]获取全局ViewType。
-     */
-    private fun saveItemViewType(holder: ViewHolder) {
-        viewType = holder.itemViewType
-    }
-
-    /**
-     * 将ViewHolder回收上限设为1，防止回收多余的ViewHolder
-     */
-    private fun RecyclerView.setMaxRecycledViews() {
-        recycledViewPool.setMaxRecycledViews(viewType, 1)
-    }
-
-    /**
-     * 清除已分离的[viewHolder]
-     */
-    private fun RecyclerView.clearDetachedViewHolder() {
-        // 调用setMaxRecycledViews()，将max设为0无法清除ScrapData，
-        // 因此直接访问mScrap，清除viewType对应的ScrapData。
-        recycledViewPool.mScrap.remove(viewType)
-        val holder = viewHolder
-        if (holder == null || holder.itemView.isAttachedToWindow) {
-            return
-        }
-        val views: ArrayList<ViewHolder> = when {
-            !holder.isScrap -> mRecycler.mCachedViews
-            !holder.mInChangeScrap -> mRecycler.mAttachedScrap
-            else -> mRecycler.mChangedScrap
-        } ?: return
-        if (views.remove(holder)) {
-            viewHolder = null
+        tracker.viewHolder?.let { holder ->
+            tracker.untrack(holder)
+            makeViewHolderRecycleFailed(holder)
         }
     }
 
     /**
-     * 让[viewHolder]回收失败
+     * 让[holder]回收失败
      *
-     * 在[onFailedToRecycleView]中对[viewHolder]的状态做进一步处理。
+     * 在[onFailedToRecycleView]中对[holder]的状态做进一步处理。
      *
      * **注意**：在[Recycler.addViewHolderToRecycledViewPool]的流程中，
      * 虽然可以通过[RecyclerListener]或者[onViewRecycled]，将回收上限设为0，
-     * 防止[viewHolder]被回收，但这种处理方式仍然会创建[ScrapData]，导致清除的不够彻底。
+     * 防止[holder]被回收，但这种处理方式仍然会创建[ScrapData]，导致清除的不够彻底。
      */
-    private fun makeViewHolderRecycleFailed() {
-        val holder = viewHolder
-        if (isAdapterAttached || holder == null) {
-            return
-        }
-        // 注意：holder.setIsRecyclable()不是单纯的设置状态，还包含计数逻辑，
-        // 若此处调用holder.setIsRecyclable(false)，则移除动画结束时，将不会移除itemView。
+    private fun makeViewHolderRecycleFailed(holder: ViewHolder) {
+        if (tracker.isAttached) return
+        // 注意：holder.setIsRecyclable()是计数逻辑，
+        // 若此处调用holder.setIsRecyclable(false)，
+        // 则移除动画结束时，RecyclerView不会移除itemView。
         holder.itemView.apply {
             if (hasTransientState) {
                 // 可能正在执行属性动画，先取消动画
@@ -128,7 +81,6 @@ internal class ViewController : View.OnAttachStateChangeListener {
                 hasTransientState = true
             }
         }
-        viewHolder = null
     }
 
     /**
@@ -138,26 +90,114 @@ internal class ViewController : View.OnAttachStateChangeListener {
      * 回收失败的详细流程[Recycler.recycleViewHolderInternal]。
      */
     fun onFailedToRecycleView(holder: ViewHolder): Boolean {
-        if (isAdapterAttached) {
-            return false
-        }
+        if (tracker.isAttached) return false
         holder.itemView.apply {
             if (hasTransientState) {
                 hasTransientState = false
             }
         }
-        // 将holder设为不可回收，是为了防止hasTransientState重置为false后，
-        // holder.isRecyclable()为true，导致仍然可以回收viewHolder。
+        // 将holder设为不可回收，防止hasTransientState重置为false后，
+        // holder.isRecyclable()为true，导致holder仍然可以被回收。
         holder.setIsRecyclable(false)
         return false
+    }
+
+    /**
+     * 清除已分离的[Tracker.viewType]和[Tracker.viewHolder]
+     *
+     * **注意**：调用[RecycledViewPool.setMaxRecycledViews]，
+     * 将`max`设为0无法清除[RecycledViewPool.ScrapData]，
+     * 因此直接访问[RecycledViewPool.mScrap]进行清除。
+     */
+    private fun RecyclerView.clearDetachedViewHolder() {
+        recycledViewPool.mScrap.remove(tracker.viewType)
+        val holder = tracker.viewHolder
+        if (holder == null || holder.itemView.isAttachedToWindow) {
+            return
+        }
+        val views: ArrayList<ViewHolder> = when {
+            !holder.isScrap -> mRecycler.mCachedViews
+            !holder.mInChangeScrap -> mRecycler.mAttachedScrap
+            else -> mRecycler.mChangedScrap
+        } ?: return
+        views.remove(holder)
     }
 
     @VisibleForTesting
     fun getRecycledViewHolder(): ViewHolder? {
         val mScrapHeap = recyclerView
-            ?.recycledViewPool?.mScrap?.get(viewType)
+            ?.recycledViewPool?.mScrap?.get(tracker.viewType)
             ?.mScrapHeap.takeIf { !it.isNullOrEmpty() } ?: return null
         require(mScrapHeap.size == 1)
         return mScrapHeap.first()
+    }
+
+    @Suppress("SpellCheckingInspection")
+    private inner class Tracker : View.OnAttachStateChangeListener {
+        /**
+         * 由于[ViewHolder.setIsRecyclable]是计数逻辑，因此用该属性确保逻辑对称
+         */
+        private var isDetachedRecyclable = false
+
+        /**
+         * 当[ConcatAdapter]使用隔离ViewType配置时，[Adapter.getItemViewType]获取到的是本地ViewType，
+         * 并不是[RecycledViewPool]中的ViewType，因此通过[ViewHolder.getItemViewType]获取全局ViewType。
+         */
+        var viewType = INVALID_TYPE
+            private set
+        var viewHolder: ViewHolder? = null
+            private set
+        var recyclerView: RecyclerView? = null
+            private set
+        val isAttached: Boolean
+            get() = recyclerView != null
+
+        fun track(holder: ViewHolder) {
+            assert(viewHolder == null) { "track出现断言异常" }
+            viewType = holder.itemViewType
+            viewHolder = holder
+        }
+
+        fun untrack(holder: ViewHolder) {
+            assert(viewHolder == holder) { "untrack出现断言异常" }
+            // 保留viewType，用于后续清除Scrap
+            viewType = holder.itemViewType
+            viewHolder = null
+        }
+
+        fun track(recyclerView: RecyclerView) {
+            assert(this.recyclerView == null) { "track出现断言异常" }
+            this.recyclerView = recyclerView
+            recyclerView.addOnAttachStateChangeListener(this)
+        }
+
+        fun untrack(recyclerView: RecyclerView) {
+            assert(this.recyclerView == recyclerView) { "untrack出现断言异常" }
+            this.recyclerView = null
+            recyclerView.removeOnAttachStateChangeListener(this)
+        }
+
+        /**
+         * 将[viewType]的回收上限设为1，防止回收多余的ViewHolder
+         */
+        fun limitMaxScrap(rv: RecyclerView) {
+            if (viewType == INVALID_TYPE) return
+            rv.recycledViewPool.setMaxRecycledViews(viewType, /*max*/1)
+        }
+
+        override fun onViewAttachedToWindow(view: View) {
+            if (isDetachedRecyclable) {
+                isDetachedRecyclable = false
+                viewHolder?.setIsRecyclable(true)
+            }
+        }
+
+        override fun onViewDetachedFromWindow(view: View) {
+            if (!isDetachedRecyclable && viewHolder?.isRecyclable == true) {
+                isDetachedRecyclable = true
+                viewHolder?.setIsRecyclable(false)
+            }
+            (view as? RecyclerView)?.clearDetachedViewHolder()
+        }
     }
 }
