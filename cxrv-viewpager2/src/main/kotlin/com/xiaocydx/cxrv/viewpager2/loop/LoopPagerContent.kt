9f4e701080d698801ee438c74bdb5a1af8c08137
xcc <xiaocydx@163.com>
2023-05-15 00:29:54 +0800

    perf: IntRange调整为返回数值

--- cxrv-viewpager2/src/main/kotlin/com/xiaocydx/cxrv/viewpager2/loop/LoopPagerContent.kt
+++ cxrv-viewpager2/src/main/kotlin/com/xiaocydx/cxrv/viewpager2/loop/LoopPagerContent.kt
@@ -23,22 +23,25 @@ import androidx.viewpager2.widget.ViewPager2
 /**
  * [ViewPager2]循环页面的内容，提供`position`的计算函数
  *
- * 以A和B是原始页面，`B*`和`A*`是额外页面为例：
+ * 例如A、B、C是原始页面，带`*`的是附加页面，[extraPageLimit] = `2`：
  * ```
- * {B* ，A ，B ，A*}
+ * {B* ，C* ，A ，B ，C ，A* ，B*}
+ *
+ * currentCount = 3
+ * supportLoop = true
+ * layoutPositions = {0 ，1 ，2 ，3 ，4 ，5 ，6}
+ * layoutPositionRange = [0, 6]
+ * headerLayoutPositionRange = [0, 1]
+ * footerLayoutPositionRange = [5, 6]
+ *
+ * bindingAdapterPositions = {1 ，2 ，0 ，1 ，2 ，0 ，1}
+ * bindingAdapterPositionRange = [0, 2]
+ * headerBindingAdapterPositionRange = [1, 2]
+ * footerBindingAdapterPositionRange = [0, 1]
  * ```
  *
- * 此时各属性的数值：
- * * [itemCount] = 2
- * * [supportLoop] = true
- *
- * * layoutPosition = `{0 ，1 ，2 ，3}`
- * * [layoutPositionRange] = `[0, 3]`
- * * [startExtraLayoutPositionRange] = `[0, 0]`
- * * [endExtraLayoutPositionRange] = `[3, 3]`
- *
- * * bindingAdapterPosition = `{1 ，0 ，1 ，0}`
- * * [bindingAdapterPositionRange] = `[0, 1]`
+ * **注意**：此类的函数仅返回数值，不返回[IntRange]，
+ * 因为这会创建对象，对频繁调用此类函数的场景不友好。
  *
  * @author xcc
  * @date 2023/5/11
@@ -49,82 +52,149 @@ internal class LoopPagerContent(
     val extraPageLimit: Int
 ) {
 
+    init {
+        require(extraPageLimit == DEFAULT_EXTRA_PAGE_LIMIT
+                || extraPageLimit == PADDING_EXTRA_PAGE_LIMIT)
+    }
+
     /**
-     * 内容`item`数量
+     * 当前的内容`item`数量
      */
-    val itemCount: Int
+    val currentCount: Int
         get() = adapter.itemCount
 
     /**
      * 是否支持循环
+     *
+     * @param contentCount 默认按[currentCount]进行判断
      */
-    val supportLoop: Boolean
-        get() = itemCount > 1
+    fun supportLoop(contentCount: Int = currentCount) = contentCount > 1
 
     /**
      * 将`bindingAdapterPosition`转换为`layoutPosition`
      *
-     * @param bindingAdapterPosition 取值范围是[bindingAdapterPositionRange]。
+     * @param bindingAdapterPosition 取值范围是[firstBindingAdapterPosition]到[lastBindingAdapterPosition]。
+     * @param contentCount           默认按[currentCount]进行转换。
      * @return 当[bindingAdapterPosition]不处于取值范围时，返回[NO_POSITION]。
      */
-    fun toLayoutPosition(bindingAdapterPosition: Int): Int {
-        if (bindingAdapterPosition !in bindingAdapterPositionRange()) return NO_POSITION
+    fun toLayoutPosition(bindingAdapterPosition: Int, contentCount: Int = currentCount): Int {
+        val first = firstBindingAdapterPosition(contentCount)
+        val last = lastBindingAdapterPosition(contentCount)
+        if (first == NO_POSITION || bindingAdapterPosition !in first..last) return NO_POSITION
         var layoutPosition = bindingAdapterPosition
-        if (supportLoop) layoutPosition += extraPageLimit
+        if (supportLoop(contentCount)) layoutPosition += extraPageLimit
         return layoutPosition
+    }
+
+    /**
+     * `layoutPosition`取值范围第一个值
+     *
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0，则返回[NO_POSITION]。
+     */
+    fun firstLayoutPosition(contentCount: Int = currentCount): Int {
+        return if (contentCount <= 0) NO_POSITION else 0
+    }
+
+    /**
+     * `layoutPosition`取值范围最后的值
+     *
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0，则返回[NO_POSITION]。
+     */
+    fun lastLayoutPosition(contentCount: Int = currentCount): Int {
+        if (contentCount <= 0) return NO_POSITION
+        val extraCount = if (!supportLoop(contentCount)) 0 else extraPageLimit * 2
+        return contentCount + extraCount - 1
+    }
+
+    /**
+     * 附加页面的`layoutPosition`取值范围第一个值
+     *
+     * @param isHeader     `true`-头部，`false`-尾部
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0或不支持循环，则返回[NO_POSITION]。
+     */
+    fun firstExtraLayoutPosition(isHeader: Boolean, contentCount: Int = currentCount): Int {
+        if (contentCount <= 0 || !supportLoop(contentCount)) return NO_POSITION
+        return if (isHeader) 0 else extraPageLimit + contentCount
+    }
+
+    /**
+     * 附加页面的`layoutPosition`取值范围最后的值
+     *
+     * @param isHeader     `true`-头部，`false`-尾部
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0或不支持循环，则返回[NO_POSITION]。
+     */
+    fun lastExtraLayoutPosition(isHeader: Boolean, contentCount: Int = currentCount): Int {
+        if (contentCount <= 0 || !supportLoop(contentCount)) return NO_POSITION
+        return if (isHeader) extraPageLimit - 1 else extraPageLimit + contentCount + extraPageLimit - 1
     }
 
     /**
      * 将`layoutPosition`转换为`bindingAdapterPosition`
      *
-     * @param layoutPosition 取值范围是[layoutPositionRange]。
+     * @param layoutPosition 取值范围是[firstLayoutPosition]到[firstLayoutPosition]。
+     * @param contentCount   默认按[currentCount]进行转换。
      * @return 当[layoutPosition]不处于取值范围时，返回[NO_POSITION]。
      */
-    fun toBindingAdapterPosition(layoutPosition: Int): Int {
-        if (layoutPosition !in layoutPositionRange()) return NO_POSITION
-        if (!supportLoop) return layoutPosition
-        var bindingAdapterPosition = (layoutPosition - extraPageLimit) % itemCount
-        if (bindingAdapterPosition < 0) bindingAdapterPosition += itemCount
+    fun toBindingAdapterPosition(layoutPosition: Int, contentCount: Int = currentCount): Int {
+        val first = firstLayoutPosition(contentCount)
+        val last = lastLayoutPosition(contentCount)
+        if (first == NO_POSITION || layoutPosition !in first..last) return NO_POSITION
+        if (!supportLoop(contentCount)) return layoutPosition
+        var bindingAdapterPosition = (layoutPosition - extraPageLimit) % currentCount
+        if (bindingAdapterPosition < 0) bindingAdapterPosition += currentCount
         return bindingAdapterPosition
     }
 
     /**
-     * `layoutPosition`的取值范围
+     * `bindingAdapterPosition`取值范围第一个值
      *
-     * @param contentCount 默认按当前`adapter.itemCount`计算取值范围
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0，则返回[NO_POSITION]。
      */
-    fun layoutPositionRange(contentCount: Int = itemCount): IntRange {
-        val extraCount = if (!supportLoop) 0 else extraPageLimit * 2
-        return 0 until contentCount + extraCount
+    fun firstBindingAdapterPosition(contentCount: Int = currentCount): Int {
+        return if (contentCount <= 0) NO_POSITION else 0
     }
 
     /**
-     * 起始端额外页面的`layoutPosition`取值范围
+     * `bindingAdapterPosition`取值范围最后的值
      *
-     * @param contentCount 默认按当前`adapter.itemCount`计算取值范围
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0，则返回[NO_POSITION]。
      */
-    fun startExtraLayoutPositionRange(contentCount: Int = itemCount): IntRange {
-        val extraCount = if (!supportLoop) 0 else extraPageLimit
-        return 0 until extraCount
+    fun lastBindingAdapterPosition(contentCount: Int = currentCount): Int {
+        return if (contentCount <= 0) NO_POSITION else contentCount - 1
     }
 
     /**
-     * 结束端额外页面的`layoutPosition`取值范围
+     * 附加页面的`bindingAdapterPosition`取值范围第一个值
      *
-     * @param contentCount 默认按当前`adapter.itemCount`计算取值范围
+     * @param isHeader     `true`-头部，`false`-尾部
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0或不支持循环，则返回[NO_POSITION]。
      */
-    fun endExtraLayoutPositionRange(contentCount: Int = itemCount): IntRange {
-        val extraCount = if (!supportLoop) 0 else extraPageLimit
-        val first = extraCount + contentCount
-        return first until first + extraCount
+    fun firstExtraBindingAdapterPosition(isHeader: Boolean, contentCount: Int = currentCount): Int {
+        if (contentCount <= 0 || !supportLoop(contentCount)) return NO_POSITION
+        return if (isHeader) contentCount - extraPageLimit else 0
     }
 
     /**
-     * `bindingAdapterPosition`的取值范围
+     * 附加页面的`bindingAdapterPosition`取值范围最后的值
      *
-     * @param contentCount 默认按当前`adapter.itemCount`计算取值范围
+     * @param isHeader     `true`-头部，`false`-尾部
+     * @param contentCount 默认按[currentCount]计算取值范围。
+     * @return 若[contentCount]小于等于0或不支持循环，则返回[NO_POSITION]。
      */
-    fun bindingAdapterPositionRange(contentCount: Int = itemCount): IntRange {
-        return 0 until contentCount
+    fun lastExtraBindingAdapterPosition(isHeader: Boolean, contentCount: Int = currentCount): Int {
+        if (contentCount <= 0 || !supportLoop(contentCount)) return NO_POSITION
+        return if (isHeader) contentCount - 1 else extraPageLimit - 1
+    }
+
+    companion object {
+        const val DEFAULT_EXTRA_PAGE_LIMIT = 1
+        const val PADDING_EXTRA_PAGE_LIMIT = 2
     }
 }
