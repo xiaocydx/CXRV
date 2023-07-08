8da52eac4c87d71c10b3201802deabd690943b90
xcc <xiaocydx@163.com>
2023-07-08 20:45:46 +0800

    perf: 简化增加回收上限的策略

--- cxrv/src/main/kotlin/com/xiaocydx/cxrv/recycle/RecycleAllViews.kt
+++ cxrv/src/main/kotlin/com/xiaocydx/cxrv/recycle/RecycleAllViews.kt
@@ -20,24 +20,23 @@
 package androidx.recyclerview.widget
 
 import android.os.Parcelable
-import android.util.SparseArray
-import android.util.SparseIntArray
 import android.view.View
 import android.view.View.OnAttachStateChangeListener
 import androidx.recyclerview.widget.RecyclerView.*
-import androidx.recyclerview.widget.RecyclerView.RecycledViewPool.ScrapData
 import com.xiaocydx.cxrv.list.Disposable
 
 /**
  * 将[RecyclerView]的子View和离屏缓存回收进[RecycledViewPool]
  *
  * ### 增加回收上限
- * 当回收数量超过`viewType`的回收上限时，
- * 调用[increaseMaxScrap]获取增加后的回收上限。
- *
- * ### 恢复回收上限
- * 若在回收过程中，调用了[increaseMaxScrap]并设置了增加后的回收上限，
- * 则在回收完成后，会恢复`viewType`原本的回收上限，避免后续缓存堆积。
+ * 当回收数量超过`viewType`的回收上限时，调用[increaseMaxScrap]获取增加后的回收上限，
+ * 回收流程会先对`viewType`的回收上限不断`+1`，当达到增加后的回收上限时，则不再`+1`，
+ * 这样做的目的是避免后续缓存冗余的[ViewHolder]。
+ * ```
+ * // viewType增加后的回收上限返回20，如果实际只回收了10个ViewHolder，
+ * // 那么回收上限只增加到10，而不是20，避免后续缓存冗余的ViewHolder。
+ * recyclerView.recycleAllViews { viewType, currentMaxScrap, initialState -> 20 }
+ * ```
  *
  * ### 确保执行回调
  * 该函数确保执行回收流程相关的回调，回调包括不限于：
@@ -53,79 +52,94 @@ fun RecyclerView.recycleAllViews(increaseMaxScrap: IncreaseMaxScrap) {
 }
 
 /**
- * 当[RecyclerView]从Window上分离时：
- * 1. 保存[LayoutManager]的状态，当再次添加到Window上或者重建时，能够恢复滚动位置。
- * 2. 调用[recycleAllViews]，将子View和离屏缓存回收进[RecycledViewPool]。
+ * 当[RecyclerView]从Window分离时，通过[recycleAllViews]将子View和离屏缓存回收进[RecycledViewPool]
  *
  * **注意**：由于[StaggeredGridLayoutManager.onDetachedFromWindow]的清除逻辑，
  * 比[View.OnAttachStateChangeListener.onViewDetachedFromWindow]先执行，
- * 因此导致该函数无法保存正确的状态，恢复的滚动位置带有一定程度的偏移。
- * 此时需要搭配[StaggeredGridLayoutManagerCompat.isSaveStateOnDetach]使用：
+ * 因此`saveState = true`无法保存正确的状态，恢复的滚动位置带有一定程度的偏移。
+ * 此时需要调用者自行保存状态，若已设置[StaggeredGridLayoutManagerCompat]，
+ * 则不需要自行保存状态，[StaggeredGridLayoutManagerCompat]已兼容这种情况：
  * ```
- * recyclerView.staggered(spanCount) { isSaveStateOnDetach = true }
+ * recyclerView.staggered(spanCount) // 设置StaggeredGridLayoutManagerCompat
  * recyclerView.setRecycleAllViewsOnDetach(increaseMaxScrap)
  * ```
  *
  * @param increaseMaxScrap 获取`viewType`增加后的回收上限。
- * @return 调用[Disposable.dispose]可以移除设置的视图回收处理。
+ * @param saveState `true`表示当[RecyclerView]从Window分离时，
+ * 保存[LayoutManager]的状态，当再次添加到Window或者重建时，能够恢复滚动位置。
+ * @return 调用[Disposable.dispose]可以移除设置的回收处理。
  */
-fun RecyclerView.setRecycleAllViewsOnDetach(increaseMaxScrap: IncreaseMaxScrap): Disposable {
-    return RecycleAllViewsOnDetachDisposable().attach(this, increaseMaxScrap)
+fun RecyclerView.setRecycleAllViewsOnDetach(
+    saveState: Boolean = true,
+    increaseMaxScrap: IncreaseMaxScrap
+): Disposable {
+    return RecycleAllViewsOnDetachDisposable().attach(this, saveState, increaseMaxScrap)
 }
 
 /**
- * 记录[RecyclerView]的初始状态，用于[IncreaseMaxScrap]
+ * [setRecycleAllViewsOnDetach]的简化函数，适用于增加后的回收上限是固定值的场景
+ *
+ * @param maxScrap  所有`viewType`增加后的回收上限。
+ * @param saveState `true`表示当[RecyclerView]从Window分离时，
+ * 保存[LayoutManager]的状态，当再次添加到Window或者重建时，能够恢复滚动位置。
  */
-data class InitialState(val childCount: Int)
+fun RecyclerView.setRecycleAllViewsOnDetach(maxScrap: Int, saveState: Boolean = true): Disposable {
+    return setRecycleAllViewsOnDetach(saveState) { _, _, _ -> maxScrap }
+}
 
 /**
  * 回收数量超过`viewType`的回收上限，获取`viewType`增加后的回收上限
  */
 typealias IncreaseMaxScrap = (viewType: Int, currentMaxScrap: Int, initialState: InitialState) -> Int
 
+/**
+ * [RecyclerView]的初始状态，用于[IncreaseMaxScrap]
+ */
+interface InitialState {
+    /**
+     * `recyclerView.layoutManager.itemCount`
+     */
+    val itemCount: Int
+
+    /**
+     * `recyclerView.childCount`
+     */
+    val childCount: Int
+}
+
 private class RecycleAllViewsRunner(
     private val rv: RecyclerView,
     private val increaseMaxScrap: IncreaseMaxScrap
-) : RecyclerListener {
-    private var state: Any? = null
-    private val initialState = InitialState(rv.childCount)
-    private val scrap: SparseArray<ScrapData> = rv.recycledViewPool.mScrap
+) : RecyclerListener, InitialState {
+    override var itemCount = 0
+    override var childCount = 0
 
     /**
-     * [ViewHolder]在被回收进[RecycledViewPool]之前，
-     * 会先执行[onViewRecycled]，尝试增加`viewType`的回收上限，
-     * 在回收完成后，恢复`viewType`原本的回收上限，避免后续缓存堆积。
+     * 在[ViewHolder]被回收进[RecycledViewPool]之前，
+     * 先执行[onViewRecycled]，尝试增加`viewType`的回收上限。
      */
     fun run() {
+        itemCount = rv.layoutManager?.itemCount ?: 0
+        childCount = rv.childCount
         val recycler = rv.mRecycler
         rv.addRecyclerListener(this)
         rv.layoutManager?.removeAndRecycleAllViews(recycler)
         recycler.clear()
         rv.removeRecyclerListener(this)
-        restoreMaxScrap()
     }
 
     override fun onViewRecycled(holder: ViewHolder) {
         holder.clearReference()
         val viewType = holder.itemViewType
+        val scrap = rv.recycledViewPool.mScrap
         val scrapData = scrap[viewType] ?: return
 
+        // 若可以继续回收，则不需要增加回收上限
         val currentMaxScrap = scrapData.mMaxScrap
-        if (scrapData.mScrapHeap.size < currentMaxScrap) {
-            // 还可以继续回收
-            return
-        }
+        if (scrapData.mScrapHeap.size < currentMaxScrap) return
 
-        val newMaxScrap = increaseMaxScrap(viewType, currentMaxScrap, initialState)
-        if (currentMaxScrap >= newMaxScrap) {
-            return
-        }
-
-        if (!containsMaxScrap(viewType)) {
-            // 同一种viewType只会进入该分支逻辑一次
-            saveMaxScrap(viewType, currentMaxScrap)
-        }
-        scrapData.mMaxScrap = newMaxScrap
+        val expectMaxScrap = increaseMaxScrap(viewType, currentMaxScrap, this)
+        if (currentMaxScrap < expectMaxScrap) scrapData.mMaxScrap = currentMaxScrap + 1
     }
 
     private fun ViewHolder.clearReference() {
@@ -134,61 +148,27 @@ private class RecycleAllViewsRunner(
         // chain：StaggeredGridLayoutManager.mRecyclerView -> Span
         lp.mSpan = null
     }
-
-    private fun containsMaxScrap(viewType: Int): Boolean {
-        return when (val state = state) {
-            is Pair -> state.viewType == viewType
-            is SparseIntArray -> state[viewType] != 0
-            else -> false
-        }
-    }
-
-    private fun saveMaxScrap(viewType: Int, maxScrap: Int) {
-        when (val state = state) {
-            null -> this.state = Pair(viewType, maxScrap)
-            is Pair -> SparseIntArray().also {
-                this.state = it
-                it.put(state.viewType, state.maxScrap)
-                it.put(viewType, maxScrap)
-            }
-            is SparseIntArray -> state.put(viewType, maxScrap)
-        }
-    }
-
-    private fun restoreMaxScrap() {
-        when (val state = state) {
-            is Pair -> restoreMaxScrap(state.viewType, state.maxScrap)
-            is SparseIntArray -> {
-                for (index in 0 until state.size()) {
-                    val viewType = state.keyAt(index)
-                    val maxScrap = state.valueAt(index)
-                    restoreMaxScrap(viewType, maxScrap)
-                }
-            }
-        }
-    }
-
-    private fun restoreMaxScrap(viewType: Int, maxScrap: Int) {
-        scrap[viewType]?.mMaxScrap = maxScrap
-    }
-
-    private class Pair(val viewType: Int, val maxScrap: Int)
 }
 
 private class RecycleAllViewsOnDetachDisposable : OnAttachStateChangeListener, Disposable {
+    private var saveState = false
     private var rv: RecyclerView? = null
-    private var increaseMaxScrap: IncreaseMaxScrap? = null
+    private var runner: RecycleAllViewsRunner? = null
     private var pendingSavedState: Parcelable? = null
+    private var canDisableStaggeredCompat = false
     override val isDisposed: Boolean
-        get() = rv == null && increaseMaxScrap == null
+        get() = rv == null && runner == null
 
     fun attach(
         rv: RecyclerView,
+        saveState: Boolean,
         increaseMaxScrap: IncreaseMaxScrap
     ): Disposable {
         this.rv = rv
-        this.increaseMaxScrap = increaseMaxScrap
+        this.saveState = saveState
+        this.runner = RecycleAllViewsRunner(rv, increaseMaxScrap)
         rv.addOnAttachStateChangeListener(this)
+        tryEnableStaggeredCompat()
         return this
     }
 
@@ -202,18 +182,37 @@ private class RecycleAllViewsOnDetachDisposable : OnAttachStateChangeListener, D
 
     override fun onViewDetachedFromWindow(view: View) {
         val rv = view as? RecyclerView ?: return
-        val lm = rv.layoutManager ?: return
-        pendingSavedState = lm.onSaveInstanceState()
-        // 这是一种取巧的做法，对LayoutManager实现类的mPendingSavedState赋值，
-        // 确保Fragment销毁时能保存状态，Fragment重建时恢复RecyclerView的滚动位置。
-        pendingSavedState?.let(lm::onRestoreInstanceState)
-        increaseMaxScrap?.let(rv::recycleAllViews)
+        val lm = rv.layoutManager
+        if (lm != null && saveState) {
+            pendingSavedState = lm.onSaveInstanceState()
+            // 这是一种取巧的做法，对LayoutManager实现类的mPendingSavedState赋值，
+            // 确保Fragment销毁时能保存状态，Fragment重建时恢复RecyclerView的滚动位置。
+            pendingSavedState?.let(lm::onRestoreInstanceState)
+        }
+        runner?.run()
+    }
+
+    private fun tryEnableStaggeredCompat() {
+        val lm = rv?.layoutManager as? StaggeredGridLayoutManagerCompat
+        if (saveState && lm != null) {
+            saveState = false
+            canDisableStaggeredCompat = !lm.isSaveStateOnDetach
+            lm.isSaveStateOnDetach = true
+        }
+    }
+
+    private fun tryDisableStaggeredCompat() {
+        val lm = rv?.layoutManager as? StaggeredGridLayoutManagerCompat
+        if (canDisableStaggeredCompat && lm != null) {
+            lm.isSaveStateOnDetach = false
+        }
     }
 
     override fun dispose() {
+        tryDisableStaggeredCompat()
         rv?.removeOnAttachStateChangeListener(this)
         rv = null
-        increaseMaxScrap = null
+        runner = null
         pendingSavedState = null
     }
 }
