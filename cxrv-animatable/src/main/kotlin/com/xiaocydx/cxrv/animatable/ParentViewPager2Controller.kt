c3679f09d29c5297f44dcc864dd042f77dd17d87
xcc <xiaocydx@163.com>
2023-07-10 13:31:19 +0800

    feat: 添加默认查找处理

--- cxrv-animatable/src/main/kotlin/com/xiaocydx/cxrv/animatable/ParentViewPager2Controller.kt
+++ cxrv-animatable/src/main/kotlin/com/xiaocydx/cxrv/animatable/ParentViewPager2Controller.kt
@@ -25,39 +25,61 @@ import androidx.recyclerview.widget.RecyclerView.SCROLL_STATE_IDLE
 import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.xiaocydx.cxrv.list.Disposable
-import java.lang.ref.WeakReference
 
 /**
  * 添加受父级[viewPager2]滚动控制的[AnimatableController]
  */
 fun AnimatableMediator.controlledByParentViewPager2(viewPager2: ViewPager2): Disposable {
+    return controlledByParentViewPager2 { viewPager2 }
+}
+
+/**
+ * 添加受父级[ViewPager2]滚动控制的[AnimatableController]
+ *
+ * @param provider 提供[ViewPager2]，默认查找最接近的父级[ViewPager2]
+ */
+fun AnimatableMediator.controlledByParentViewPager2(
+    provider: ViewPager2Provider = defaultViewPager2Provider
+): Disposable {
     findAnimatableController<ParentViewPager2Controller>()?.dispose()
-    return ParentViewPager2Controller().attach(this, viewPager2)
+    return ParentViewPager2Controller().attach(this, provider)
+}
+
+typealias ViewPager2Provider = (view: View) -> ViewPager2?
+
+private val defaultViewPager2Provider = { view: View ->
+    var parent: View? = view.parent as? View
+    // parent是ViewPager2，表示当前View是ViewPager2.mRecyclerView
+    if (parent is ViewPager2) parent = parent.parent as? View
+    while (parent != null && parent !is ViewPager2) {
+        parent = parent.parent as? View
+    }
+    parent as? ViewPager2
 }
 
 private class ParentViewPager2Controller : OnPageChangeCallback(),
         OnAttachStateChangeListener, AnimatableController {
     private var mediator: AnimatableMediator? = null
-    private var viewPager2Ref: WeakReference<ViewPager2>? = null
-    private var isRegisteredCallback = false
-    private val viewPager2: ViewPager2?
-        get() = viewPager2Ref?.get()
+    private var provider: ViewPager2Provider? = null
+    private var viewPager2: ViewPager2? = null
     override val isDisposed: Boolean
-        get() = mediator == null && viewPager2 == null
+        get() = mediator == null && provider == null
     override val canStartAnimatable: Boolean
         get() = viewPager2?.let { it.scrollState == SCROLL_STATE_IDLE } ?: true
 
     fun attach(
         mediator: AnimatableMediator,
-        viewPager2: ViewPager2
+        provider: ViewPager2Provider
     ): Disposable {
         this.mediator = mediator
-        this.viewPager2Ref = WeakReference(viewPager2)
+        this.provider = provider
         mediator.also {
             it.addAnimatableController(this)
             it.recyclerView.addOnAttachStateChangeListener(this)
         }
-        registerOnPageChangeCallback()
+        if (mediator.recyclerView.isAttachedToWindow) {
+            registerOnPageChangeCallback()
+        }
         return this
     }
 
@@ -89,15 +111,16 @@ private class ParentViewPager2Controller : OnPageChangeCallback(),
     }
 
     private fun registerOnPageChangeCallback() {
-        if (isRegisteredCallback) return
-        isRegisteredCallback = true
+        if (viewPager2 != null) return
+        val rvChild = mediator?.recyclerView
+        viewPager2 = rvChild?.let { provider?.invoke(it) }
         viewPager2?.registerOnPageChangeCallback(this)
     }
 
     private fun unregisterOnPageChangeCallback() {
-        if (!isRegisteredCallback) return
-        isRegisteredCallback = false
+        if (viewPager2 == null) return
         viewPager2?.unregisterOnPageChangeCallback(this)
+        viewPager2 = null
     }
 
     override fun dispose() {
@@ -107,6 +130,6 @@ private class ParentViewPager2Controller : OnPageChangeCallback(),
         }
         unregisterOnPageChangeCallback()
         mediator = null
-        viewPager2Ref = null
+        provider = null
     }
 }
