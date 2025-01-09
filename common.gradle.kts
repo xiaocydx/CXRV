525647e9dc451e64394a497d5745c2a273fdd7d3
xcc <xiaocydx@163.com>
2025-01-09 16:18:28 +0800

    feat: 支持在Android 14运行单元测试

--- common.gradle.kts
+++ common.gradle.kts
@@ -65,7 +65,7 @@ class CommonPlugin : Plugin<Project> {
 
     private companion object {
         const val COMPILE_SDK = 33
-        const val MIN_SDK = 21
+        const val MIN_SDK = 23
         const val TARGET_SDK = 33
     }
 }
