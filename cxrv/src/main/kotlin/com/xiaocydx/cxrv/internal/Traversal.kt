7093e6ea49d33dcfafcdbf00895cea045471c079
xcc <xiaocydx@163.com>
2025-01-10 09:44:00 +0800

    perf: 简化判断逻辑

--- cxrv/src/main/kotlin/com/xiaocydx/cxrv/internal/Traversal.kt
+++ cxrv/src/main/kotlin/com/xiaocydx/cxrv/internal/Traversal.kt
@@ -32,9 +32,6 @@ import com.xiaocydx.cxrv.list.reverseAccessEach
 
 /**
  * 若当前在doFrame消息中，则结果表示当前帧的Vsync时间
- *
- * 若在doFrame消息中，则`AnimationUtils.currentAnimationTimeMillis() <= SystemClock.uptimeMillis()`，
- * `=`是因为[Choreographer]设置完`AnimationUtils.currentAnimationTimeMillis()`就开始执行各类Callback。
  */
 internal val currentAnimationTimeNanos: Long
     get() = AnimationUtils.currentAnimationTimeMillis() * 1_000_000
@@ -108,10 +105,10 @@ private class BroadcastProxy(private val view: View) {
         broadcastFrame?.unregister(this)
         broadcastFrame = null
         // this或broadcastFrame被移除，同步执行callback
-        dispatchTraversalCallbacks()
+        consumeTraversalCallbacks()
     }
 
-    fun dispatchTraversalCallbacks() {
+    fun consumeTraversalCallbacks() {
         val frameTimeNanos = currentAnimationTimeNanos
         callbacks.reverseAccessEach { it.doFrame(frameTimeNanos) }
         callbacks = callbacks.clear()
@@ -119,15 +116,8 @@ private class BroadcastProxy(private val view: View) {
 }
 
 private class BroadcastFrame private constructor(context: Context) : View(context) {
-    private var canDispatchUnregister = true
+    private var canUnregisterOnDetached = true
     private var proxyList = InlineList<BroadcastProxy>()
-
-    /**
-     * `parent`是[FrameLayout]才能确保按child顺序进行测量，[BroadcastFrame]是第0位child，
-     * [requestApplyInsets]和[dispatchApplyWindowInsets]为替补方案，尽可能确保child顺序。
-     */
-    private val isDispatchOnMeasure: Boolean
-        get() = parent is FrameLayout
 
     init {
         setWillNotDraw(true)
@@ -143,44 +133,44 @@ private class BroadcastFrame private constructor(context: Context) : View(contex
     }
 
     fun scheduleTraversal() {
-        if (isDispatchOnMeasure) {
-            ensureFirstMeasure()
-        } else if (!isLayoutRequested) {
-            // 调用requestApplyInsets()后，再次调用没有状态拦截，
-            // 利用requestLayout()添加的isLayoutRequested做拦截。
+        ensureFirstMeasure()
+        requestLayout()
+        if (parent !is FrameLayout) {
+            // parent通常是DecorView，DecorView继承自FrameLayout，
+            // FrameLayout.onMeasure()按照child顺序进行测量，BroadcastFrame是第0位child。
+            // requestApplyInsets()是替补方案，尽可能让BroadcastFrame在其它child之前执行。
             requestApplyInsets()
         }
-        requestLayout()
     }
 
     override fun dispatchApplyWindowInsets(insets: WindowInsets): WindowInsets {
-        if (!isDispatchOnMeasure) dispatchTraversalCallbacks()
+        consumeTraversalCallbacks()
         return insets
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        if (isDispatchOnMeasure) dispatchTraversalCallbacks()
+        consumeTraversalCallbacks()
         setMeasuredDimension(0, 0)
     }
 
-    private fun dispatchTraversalCallbacks() {
-        proxyList.reverseAccessEach { it.dispatchTraversalCallbacks() }
+    private fun consumeTraversalCallbacks() {
+        proxyList.reverseAccessEach { it.consumeTraversalCallbacks() }
     }
 
     private fun ensureFirstMeasure() {
         val parent = parent as? ViewGroup ?: return
         val first = parent.getChildAt(0)
         if (first !== this) {
-            canDispatchUnregister = false
+            canUnregisterOnDetached = false
             parent.removeView(this)
             parent.addView(this, 0)
-            canDispatchUnregister = true
+            canUnregisterOnDetached = true
         }
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        if (!canDispatchUnregister) return
+        if (!canUnregisterOnDetached) return
         proxyList.reverseAccessEach { it.unregisterFromBroadcastFrame() }
         proxyList = proxyList.clear()
     }
@@ -192,6 +182,7 @@ private class BroadcastFrame private constructor(context: Context) : View(contex
     companion object {
 
         fun get(view: View): BroadcastFrame? {
+            if (view is BroadcastFrame) return view
             val rootView = view.rootView as? ViewGroup ?: return null
             require(rootView !== view) { "view.rootView不能是view自身" }
             rootView.childEach { if (it is BroadcastFrame) return it }
